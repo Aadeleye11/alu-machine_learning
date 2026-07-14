d01371f6c9424a54e614b63972bb2e596607099b
Ayomide Adeleye <a.adeleye@alustudent.com>
2026-07-14 11:26:02 +0200

    Add grayscale convolution function with padding and stride
    Implements a convolution function for grayscale images with customizable padding and stride.

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Performs a convolution on grayscale images with custom padding and stride"""
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    images: numpy.ndarray (m, h, w) - multiple grayscale images
+    kernel: numpy.ndarray (kh, kw) - convolution kernel
+    padding: tuple (ph, pw), 'same', or 'valid'
+    stride: tuple (sh, sw) - stride for height and width
+
+    Returns: numpy.ndarray containing the convolved images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+                     mode='constant', constant_values=0)
+
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+
+    convolved = np.zeros((m, output_h, output_w))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            window = padded[:, i * sh:i * sh + kh, j * sw:j * sw + kw]
+            convolved[:, i, j] = np.sum(window * kernel, axis=(1, 2))
+
+    return convolved
