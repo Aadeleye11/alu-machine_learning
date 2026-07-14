b51bfa215df425f016fef335350a097b520876af
Ayomide Adeleye <a.adeleye@alustudent.com>
2026-07-14 11:44:05 +0200

    Simplify padding in convolution function
    Refactor padding implementation for clarity.

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -24,19 +24,12 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     else:
         ph, pw = padding
 
-    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                     mode='constant', constant_values=0)
+    pad_width = ((0, 0), (ph, ph), (pw, pw), (0, 0))
+    padded = np.pad(images, pad_width, mode='constant', constant_values=0)
 
     output_h = (h + 2 * ph - kh) // sh + 1
     output_w = (w + 2 * pw - kw) // sw + 1
 
     convolved = np.zeros((m, output_h, output_w, nc))
 
-    for i in range(output_h):
-        for j in range(output_w):
-            for k in range(nc):
-                window = padded[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
-                convolved[:, i, j, k] = np.sum(
-                    window * kernels[:, :, :, k], axis=(1, 2, 3))
-
-    return convolved
+    for i in
